0cadc97a39b1f5e15d026e4b52a004a0adbb2577
zhaolingxue <18222478537@163.com>
2018-04-25 22:08:35 +0800

    'httpServer'

--- server/Demo.js
+++ server/Demo.js
@@ -1,3 +1,12 @@
 let user = require('./User')
 console.log(`userName:${user.userName}`)
 console.log(`${user.sayHello()}`)
+let http = require('http')
+let server = http.createServer((req,res) => {
+   res.statusCode = 200
+   res.setHeader('content-Type','text/plain')
+   res.end('Hello,node.js')
+})
+server.listen(3000,'127.0.0.1', () => {
+  console.log('服务器已经运行，请打开http://127.0.0.1:3000来访问')
+})
