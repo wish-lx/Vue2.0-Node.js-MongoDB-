cc5fae9279a737d5f0ef38f8b45fa92b6d0265d0
zhaolingxue <18222478537@163.com>
2018-04-26 13:47:16 +0800

    'crearHttpServer'

--- server/Demo.js
+++ server/Demo.js
@@ -4,11 +4,12 @@ console.log(`${user.sayHello()}`)
 let http = require('http')
 let url = require('url')
 let util = require('util')
-let server = http.createServer((req,res) => {
+let server = http.createServer((req, res) => {
    res.statusCode = 200
    res.setHeader('content-Type','text/plain')
-   res.end(util.inspect(url.parse(req.url)))
+   res.end(util.inspect(url.parse('http://127.0.0.1:3000/demo.html?a=123#234')))
 })
 server.listen(3000,'127.0.0.1', () => {
   console.log('服务器已经运行，请打开http://127.0.0.1:3000来访问')
 })
+// url.parse() 字符串转成对象
